bcd09e338cabfee3741050bbfa4895f8349c6ba5
freude916 <funkecharm@gmail.com>
2026-06-08 00:46:19 +0800

    fix 107

--- QuickSaveLoad.cs
+++ QuickSaveLoad.cs
@@ -42,7 +42,7 @@ public static class QuickSaveLoad
             RunManager.Instance.CleanUp();
 
             // 3. Reload from save (same flow as NMainMenu.OnContinueButtonPressedAsync)
-            await RunManager.Instance.SetUpSavedSinglePlayer(runState, serializableRun);
+            await RunManager.Instance.SetUpSavedSingleplayer(runState, serializableRun);
             NGame.Instance.ReactionContainer.InitializeNetworking(new NetSingleplayerGameService());
             await NGame.Instance.LoadRun(runState, serializableRun.PreFinishedRoom);
             await NGame.Instance.Transition.FadeIn();
